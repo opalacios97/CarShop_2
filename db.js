3df2e3f9bfcf2bf5c0080ee2ead6b11f698b1296
opalacios97 <122688371+opalacios97@users.noreply.github.com>
2023-08-01 14:50:32 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const USER = 'leonardolarrea1';
-const PASSWORD = 'RfII1K1caoBJYJVr';
-const DATABASE_NAME = 'practica3';
+const USER = 'felixopm1997';
+const PASSWORD = 'tgEoojrJElG78W25';
+const DATABASE_NAME = 'Proyecto';
 
 
 const URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.ntz7wrc.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
@@ -15,37 +15,6 @@ mongoose.connect(URL)
 });
 
 
-
-// const bookings = mongoose.Schema({
-//     first_name: String,
-//     last_name: String,
-//     address: String,
-//     city: String
-// });
-
-
-// const Bookings = new mongoose.model('booking', bookings);
-
-// const newbooking = new Bookings({
-//     first_name: 'Leonardo',
-//     last_name: 'Larrea',
-//     address: 'Kennedy Norte',
-//     city: 'Guayaquil'
-// })
-// .save()
-// .then((b) => {
-//     console.log('Booking:', b);
-// })
-// .catch((error) => {
-//     console.log('Error:', error);
-// })
-// Bookings.find({ first_name: 'Eduardo'})
-// .then((b) => {
-//  console.log('Booking:', b)
-// })
-// .catch((error) => {
-//  console.log('Error:', error);
-// })
 
 
 
