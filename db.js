99de34229e2124e7a55a33722523adb5610241cd
opalacios97 <122688371+opalacios97@users.noreply.github.com>
2023-08-01 20:01:11 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const USER = 'felixopm1997';
-const PASSWORD = 'tgEoojrJElG78W25';
+const PASSWORD = '6SZZMdYiYP9jqanX';
 const DATABASE_NAME = 'Proyecto';
 
 
-const URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.ntz7wrc.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
+const URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.jnjvrqv.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(URL)
 .then(() => {
     console.log('Database connected!');
@@ -13,9 +13,6 @@ mongoose.connect(URL)
 .catch((error)=> {
     console.log('Error connecting:', error);
 });
-
-
-
 
 
 
